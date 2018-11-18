5c9717786ecadcca9c9236894c0f6a4c5e4fda73
mpilopillz <mpilo.dlamini@yahoo.com>
2018-11-18 12:12:45 +0200

    learning useref

--- max-react/src/App.tsx
+++ max-react/src/App.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
 
 function App() {
   const todos = [{ id: "t1", text: "Finish the course" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
   return (
     <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
