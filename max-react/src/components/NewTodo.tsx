f4f46c86b80c55431fbf66f2f6e0683f08a5227f
mpilopillz <mpilo.dlamini@yahoo.com>
2019-11-28 12:12:45 +0200

    learning useref

--- max-react/src/components/NewTodo.tsx
+++ max-react/src/components/NewTodo.tsx
@@ -2,9 +2,13 @@ import React, { useRef } from "react";
 
 const NewTodo: React.FC = () => {
   const textInputRef = useRef<HTMLInputElement>(null);
-
+  const todoSubmitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = textInputRef.current!.value;
+    console.log(enteredText);
+  };
   return (
-    <form>
+    <form onSubmit={todoSubmitHandler}>
       <div>
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
