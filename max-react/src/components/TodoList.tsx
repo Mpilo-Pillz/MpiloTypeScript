a9f00e7a9fec535cc9b7826c4bb99f6edaa31978
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-04 20:28:08 +0200

    Adding ToDo list component

---
+++ max-react/src/components/TodoList.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+}
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}> {todo.text}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
