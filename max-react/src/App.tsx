6b2c583c985ecf6dd050500d452054a3169eb69f
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-05 20:12:45 +0200

    adding updates to app js

--- max-react/src/App.tsx
+++ max-react/src/App.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import TodoList from "./components/TodoList";
 
 function App() {
-  return <div className="App"></div>;
+  const todos = [{ id: "t1", text: "Finish the course" }];
+  return (
+    <div className="App">
+      <TodoList items={todos} />
+    </div>
+  );
 }
 
 export default App;
