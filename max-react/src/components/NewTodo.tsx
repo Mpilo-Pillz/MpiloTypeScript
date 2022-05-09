fd68e5d6eb6f45b7e7d3c98d962b2a463111b835
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-09 20:12:45 +0200

    adding use ref

--- max-react/src/components/NewTodo.tsx
+++ max-react/src/components/NewTodo.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const NewTodo: React.FC = () => {
+  const textInputRef = useRef<HTMLInputElement>(null);
+
   return (
     <form>
       <div>
         <label htmlFor="todo-text">Todo Text</label>
-        <input type="text" id="todo-text" />
+        <input type="text" id="todo-text" ref={textInputRef} />
       </div>
       <button type="submit">Add ToDO</button>
     </form>
