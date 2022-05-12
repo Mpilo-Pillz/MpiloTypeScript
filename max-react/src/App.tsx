9622c1f1f8251fc987ea1fa0f0009746b58f1e99
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-12 18:40:49 +0200

    typing state

--- max-react/src/App.tsx
+++ max-react/src/App.tsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import NewTodo from "./components/NewTodo";
 import TodoList from "./components/TodoList";
+import { Todo } from "./todo.model";
 
 function App() {
-  const todos = [{ id: "t1", text: "Finish the course" }];
+  // const todos = [{ id: "t1", text: "Finish the course" }];
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    console.log(text);
+    setTodos([{ id: Math.random().toString(), text: text }]);
   };
   return (
     <div className="App">
