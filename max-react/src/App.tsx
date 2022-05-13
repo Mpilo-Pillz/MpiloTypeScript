c78b185472f113d3532457d5ff71aa56ce15ce72
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-13 19:26:31 +0200

    adding todos model and updating todos state

--- max-react/src/App.tsx
+++ max-react/src/App.tsx
@@ -9,7 +9,10 @@ function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-    setTodos([{ id: Math.random().toString(), text: text }]);
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
   };
   return (
     <div className="App">
