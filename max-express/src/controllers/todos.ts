f76bc231dfcf01a08535e23f66094f5d0b5376d4
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-15 19:27:50 +0200

    adding get, crrat and update todo

---
+++ max-express/src/controllers/todos.ts
@@ -0,0 +1,34 @@
+import { RequestHandler } from "express";
+
+import { Todo } from "../models/todos";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+
+  res.status(201).json({ message: "Created the todo.", createTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const updatedText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("Could not find todo!");
+  }
+
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+
+  res.json({ message: "Updated", updateTodo: TODOS[todoIndex] });
+};
