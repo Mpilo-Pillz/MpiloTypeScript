be1c0e99c23ebd4f52334ed9b0564837d4bbde0a
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-15 18:43:06 +0200

    updating app.tsx

--- max-react/src/App.tsx
+++ max-react/src/App.tsx
@@ -14,10 +14,16 @@ function App() {
       { id: Math.random().toString(), text: text },
     ]);
   };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
     </div>
   );
 }
