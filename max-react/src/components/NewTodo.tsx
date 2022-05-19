8427df14386a14c3f56cec80b8a0236e2e0d97c4
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-19 21:32:50 +0200

    adding new todo component

--- max-react/src/components/NewTodo.tsx
+++ max-react/src/components/NewTodo.tsx
@@ -1,19 +1,23 @@
 import React, { useRef } from "react";
+import "./NewTodo.css";
 
 type NewTodoProps = {
   onAddTodo: (todoText: string) => void;
 };
+
 const NewTodo: React.FC<NewTodoProps> = (props) => {
   const textInputRef = useRef<HTMLInputElement>(null);
+
   const todoSubmitHandler = (event: React.FormEvent) => {
     event.preventDefault();
     const enteredText = textInputRef.current!.value;
     console.log(enteredText);
     props.onAddTodo(enteredText);
   };
+
   return (
     <form onSubmit={todoSubmitHandler}>
-      <div>
+      <div className="form-control">
         <label htmlFor="todo-text">Todo Text</label>
         <input type="text" id="todo-text" ref={textInputRef} />
       </div>
