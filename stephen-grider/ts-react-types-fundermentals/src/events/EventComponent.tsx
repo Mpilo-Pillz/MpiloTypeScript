42d89389a3a3e3419f2ad37b30d1ab8f894ff114
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-28 10:26:44 +0200

    LEarning to type drag events

--- stephen-grider/ts-react-types-fundermentals/src/events/EventComponent.tsx
+++ stephen-grider/ts-react-types-fundermentals/src/events/EventComponent.tsx
@@ -4,10 +4,18 @@ const EventComponent: React.FC = () => {
   const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     console.log(event);
   };
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log("This is a Drag");
+    console.log(event);
+  };
   return (
     <div>
       <input onChange={(e) => console.log(e)} />
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me
+      </div>
     </div>
   );
 };
