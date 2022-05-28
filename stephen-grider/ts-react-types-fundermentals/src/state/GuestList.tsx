3c78df51f140a64410c5cfa2d2efd7043c09227a
mpilopillz <mpilo.dlamini@yahoo.com>
2022-05-28 09:51:45 +0200

    addding functionality to guest list

--- stephen-grider/ts-react-types-fundermentals/src/state/GuestList.tsx
+++ stephen-grider/ts-react-types-fundermentals/src/state/GuestList.tsx
@@ -12,6 +12,11 @@ const GuestList: React.FC = () => {
   return (
     <div>
       <h3>Guest List</h3>
+      <ul>
+        {guests.map((guest) => (
+          <li key={guest}>{guest}</li>
+        ))}
+      </ul>
       <input value={name} onChange={(e) => setName(e.target.value)} />
       <button onClick={onClick}>Add Guest</button>
     </div>
