bebdbe34ecd9a0ff2af279a763a80365e804add6
mpilopillz <mpilo.dlamini@yahoo.com>
2023-02-28 19:20:45 +0200

    learning object annotations

--- tsPogramming/objectAnnotions.ts
+++ tsPogramming/objectAnnotions.ts
@@ -12,4 +12,15 @@ const profile = {
 
 const {age}: {age: number} = profile;
 
-const {coords: {lat, lng} }: {coords: {lat: number, lng: number}} = profile
+const {coords: {lat, lng} }: {coords: {lat: number, lng: number}} = profile;
+
+const importantDates: (Date | string)[] = [];
+importantDates.push('2023-12-16')
+importantDates.push(new Date());
+
+const water: [string, boolean, number] = ['clear', false, 0 ];
+// drink[0] = 20 throws  type error as it should be a string
+
+// Type Alias
+type Drink = [string, boolean, number];
+const fizzyBubble: Drink = ["red", true, 40]
