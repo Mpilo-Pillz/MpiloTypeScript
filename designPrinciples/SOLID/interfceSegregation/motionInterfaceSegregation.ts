f6d9b6d4021ae3231681afb8082588fab79974b9
mpilopillz <mpilo.dlamini@yahoo.com>
2023-03-10 19:20:45 +0200

    creating a whale

--- designPrinciples/SOLID/interfceSegregation/motionInterfaceSegregation.ts
+++ designPrinciples/SOLID/interfceSegregation/motionInterfaceSegregation.ts
@@ -27,4 +27,29 @@ class Chicken extends Bird {
     }
 }
 
+class Penguin extends Bird implements CanSwim {
+    swim(){
+        return `Slides and Dives in to te cold water`
+    };
+}
+
+const whale = {
+    breathesUnderWater: false,
+    givesBirth() {
+        return `A very tiny adorable whales now exists`
+    },
+    swim() {
+        return `Stays under water for a very long time`
+    }
+}
+
 const nkhuku = new Chicken();
+const liDada = new Penguin();
+
+const swimmingShow = (creature: CanSwim) => {
+    console.log(creature.swim())
+}
+
+swimmingShow(liDada)
+swimmingShow(whale)
+// swimmingShow(nkhuku) throws error chickens dont swim
