0e3d756bfd2426862224f0e752ec368183f63b9c
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-07 19:20:45 +0200

    learning protected

--- tsPogramming/modifiersWithVehicles.ts
+++ tsPogramming/modifiersWithVehicles.ts
@@ -1,24 +1,36 @@
 class Vehicle {
+    private kilometers = 0;
+
     public drive(): void {
         console.log("chugga chugga");
+        this.increaseCarKilometers()
+    }
+    private increaseCarKilometers() {
+        this.kilometers++
     }
 
     public honk(): void {
         console.log("Beep"); 
     }
+    protected sendPowerStoredInBatteries() {
+        console.log("Can use radio, lights, and infotainment system");
+        
+    }
 }
 
 class Car extends Vehicle {
     public drive(): void {
-        console.log("vroom"); // cant be ade private if parent has it public
+        console.log("vroom"); // cant be a private if parent has it public
+        // this.increaseCarKilometers() make modifier protected if want it o be used here
     }
-
+// marked as private becuase it is a method that we do not want other developers to call as they could break something
     private moveFuelToEngine() {
         console.log("Fuel moved form fuel tank to engine");
     }
 
     public startEngine() {
-        this.moveFuelToEngine()
+        this.moveFuelToEngine();
+        this.sendPowerStoredInBatteries()
     }
 }
 
