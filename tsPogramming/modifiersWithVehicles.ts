4be3e9feb65513ee2b8e32461d35a1ec2b149d29
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-08 19:20:45 +0200

    learning constructors

--- tsPogramming/modifiersWithVehicles.ts
+++ tsPogramming/modifiersWithVehicles.ts
@@ -1,41 +1,48 @@
 class Vehicle {
-    private kilometers = 0;
+  private kilometers = 0;
+  // color: string = 'Sonic Titanium'
+  color: string;
 
-    public drive(): void {
-        console.log("chugga chugga");
-        this.increaseCarKilometers()
-    }
-    private increaseCarKilometers() {
-        this.kilometers++
-    }
+  constructor(color: string) {
+    this.color = color;
+  }
 
-    public honk(): void {
-        console.log("Beep"); 
-    }
-    protected sendPowerStoredInBatteries() {
-        console.log("Can use radio, lights, and infotainment system");
-        
-    }
+  public drive(): void {
+    console.log("chugga chugga");
+    this.increaseCarKilometers();
+  }
+  private increaseCarKilometers() {
+    this.kilometers++;
+  }
+
+  public honk(): void {
+    console.log("Beep");
+  }
+  protected sendPowerStoredInBatteries() {
+    console.log("Can use radio, lights, and infotainment system");
+  }
 }
 
 class Car extends Vehicle {
-    public drive(): void {
-        console.log("vroom"); // cant be a private if parent has it public
-        // this.increaseCarKilometers() make modifier protected if want it o be used here
-    }
-// marked as private becuase it is a method that we do not want other developers to call as they could break something
-    private moveFuelToEngine() {
-        console.log("Fuel moved form fuel tank to engine");
-    }
+  public drive(): void {
+    console.log("vroom"); // cant be a private if parent has it public
+    // this.increaseCarKilometers() make modifier protected if want it o be used here
+  }
+  // marked as private becuase it is a method that we do not want other developers to call as they could break something
+  private moveFuelToEngine() {
+    console.log("Fuel moved form fuel tank to engine");
+  }
 
-    public startEngine() {
-        this.moveFuelToEngine();
-        this.sendPowerStoredInBatteries()
-    }
+  public startEngine() {
+    this.moveFuelToEngine();
+    this.sendPowerStoredInBatteries();
+  }
 }
+const vehicle = new Vehicle("Grey");
 
-const car = new Car();
+const car = new Car('Sonic Titanium');
 // car.moveFuelToEngine(); // private can't be accessed outside the class. only methods inside the class can call drive
-car.startEngine()
-car.drive(); 
+car.startEngine();
+car.drive();
 car.honk();
+car.color;
