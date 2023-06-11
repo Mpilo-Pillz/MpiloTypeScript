fd754b08d7528ae3e82277ec080a4b46af37d9f1
mpilopillz <mpilo.dlamini@yahoo.com>
2023-06-11 19:20:45 +0200

    learning super constructor

--- tsPogramming/modifiersWithVehicles.ts
+++ tsPogramming/modifiersWithVehicles.ts
@@ -1,9 +1,9 @@
 class Vehicle {
-  private kilometers = 0;
+//   private kilometers = 0;
   // color: string = 'Sonic Titanium'
   color: string;
 
-  constructor(color: string) {
+  constructor(color: string, private kilometers: number) {
     this.color = color;
   }
 // short syntax
@@ -26,6 +26,11 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
+
+    constructor(public wheels: number, color: string, kilometers: number) {
+        super(color, kilometers); // is a reference to constructor method in parent
+        // we didn't put put public infront of color to not overwrite the field
+    }
   public drive(): void {
     console.log("vroom"); // cant be a private if parent has it public
     // this.increaseCarKilometers() make modifier protected if want it o be used here
@@ -40,9 +45,9 @@ class Car extends Vehicle {
     this.sendPowerStoredInBatteries();
   }
 }
-const vehicle = new Vehicle("Grey");
+const vehicle = new Vehicle("Grey", 0);
 
-const car = new Car('Sonic Titanium');
+const car = new Car(4, 'Sonic Titanium', 10000);
 // car.moveFuelToEngine(); // private can't be accessed outside the class. only methods inside the class can call drive
 car.startEngine();
 car.drive();
