c6fd59dd3acb5dc1f2726a91e8e63b00c55e9ef8
mpilopillz <mpilo.dlamini@yahoo.com>
2023-07-15 12:12:45 +0200

    creating company

---
+++ stephen-grider/maps/src/Company.ts
@@ -0,0 +1,19 @@
+import { faker } from "@faker-js/faker";
+
+export class Company {
+    companyName: string;
+    catchPhrase: string;
+    location: {
+        lat: number;
+        lng: number
+    }
+
+    constructor() {
+        this.companyName = faker.company.name()
+        this.catchPhrase = faker.company.catchPhrase()
+        this.location = {
+            lat: faker.location.latitude(),
+            lng: faker.location.longitude()
+        }
+    }
+}
