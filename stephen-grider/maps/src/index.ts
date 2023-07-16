e6b6674e3cb49e66f4dcd8fa2bf67ed1936d06f5
mpilopillz <mpilo.dlamini@yahoo.com>
2023-07-16 12:12:45 +0200

    importing company

--- stephen-grider/maps/src/index.ts
+++ stephen-grider/maps/src/index.ts
@@ -1,8 +1,10 @@
+import { Company } from "./Company";
 import { User } from "./User";
 
 console.log("Not hello world");
 
 const user = new User()
+const company = new Company()
 
 console.log(user);
 
