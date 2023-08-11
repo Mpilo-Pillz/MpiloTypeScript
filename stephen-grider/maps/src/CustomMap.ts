915bbbae91df669e1e32307f774a38538a24efe1
mpilopillz <mpilo.dlamini@yahoo.com>
2023-08-11 20:58:29 +0200

    adding a fascade over map

---
+++ stephen-grider/maps/src/CustomMap.ts
@@ -0,0 +1,10 @@
+export class CustomMap {
+    private googleMap: google.maps.Map;
+
+    constructor(divId: string) {
+        this.googleMap = new google.maps.Map(document.getElementById(divId) as HTMLElement, {
+            zoom: 1,
+            center: {lat: 0, lng: 0}
+        });
+    }
+}
--- stephen-grider/maps/src/index.ts
+++ stephen-grider/maps/src/index.ts
@@ -1,17 +1,5 @@
 /// <reference types="@types/google.maps" />
-import { Company } from "./Company";
-import { User } from "./User";
 
-console.log("Not hello world");
+import { CustomMap } from "./CustomMap";
 
-const user = new User()
-const company = new Company()
-
-console.log(user);
-console.log(company);
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-    zoom: 1,
-    center: {lat: 0, lng: 0}
-});
-
+const customMap = new CustomMap('map');
