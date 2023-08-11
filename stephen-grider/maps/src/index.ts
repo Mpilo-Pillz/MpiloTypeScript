0a69bb310da753c65415a465969d153b6c170e89
mpilopillz <mpilo.dlamini@yahoo.com>
2023-08-11 20:37:08 +0200

    plotting map

--- stephen-grider/maps/src/index.ts
+++ stephen-grider/maps/src/index.ts
@@ -1,3 +1,4 @@
+/// <reference types="@types/google.maps" />
 import { Company } from "./Company";
 import { User } from "./User";
 
@@ -8,4 +9,9 @@ const company = new Company()
 
 console.log(user);
 console.log(company);
+
+new google.maps.Map(document.getElementById('map') as HTMLElement, {
+    zoom: 1,
+    center: {lat: 0, lng: 0}
+});
 
