cae0ca4a594db86b8d152755a8637c0128434c8e
Fikasentani <mpilo.dlamini@yahoo.com>
2024-01-13 21:56:21 +0200

    revising pyramid

--- codingExercises/pyramid.ts
+++ codingExercises/pyramid.ts
@@ -25,3 +25,21 @@ pyramid(5);
 // colum less than double the number -1
 // take midpoint subtract row check if its less than column
 // take midpoint and add row and check if its more than column
+
+function revisePyramid(numOfSteps: number) {
+  const midPoint = Math.floor((2 * numOfSteps - 1) / 2);
+
+  for (let row = 0; row < numOfSteps; row++) {
+    let step = "";
+    for (let column = 0; column < 2 * numOfSteps - 1; column++) {
+      if (midPoint - row <= column && midPoint + row >= column) {
+        step += "#";
+      } else {
+        step += " ";
+      }
+    }
+    console.log(step);
+  }
+}
+
+revisePyramid(10);
