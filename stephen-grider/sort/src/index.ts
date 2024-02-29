93fa7c87b5b8fb5d94413a5ebf174e1dc1cc842d
Fikasentani <mpilo.dlamini@yahoo.com>
2024-02-29 19:28:34 +0200

    Adding Sorting logic to sort numbers

--- stephen-grider/sort/src/index.ts
+++ stephen-grider/sort/src/index.ts
@@ -1,7 +1,20 @@
 class Sorter {
   constructor(public collection: number[]) {}
 
-  sort(): void {}
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        // the element is J and the element next to is is j+ 1
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
 }
 
 const sorter = new Sorter([-18, 14, -11, 5]);
