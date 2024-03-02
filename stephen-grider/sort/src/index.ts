281016675bd9752ff84600fd32972deb8ee57261
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-02 08:20:47 +0200

    checking type of

--- stephen-grider/sort/src/index.ts
+++ stephen-grider/sort/src/index.ts
@@ -1,18 +1,25 @@
 class Sorter {
-  constructor(public collection: number[]) {}
+  constructor(public collection: number[] | string) {}
 
   sort(): void {
     const { length } = this.collection;
 
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        // the element is J and the element next to is is j+ 1
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
+    // if collection is an array of numbers
+    if (this.collection instanceof Array) {
+      for (let i = 0; i < length; i++) {
+        for (let j = 0; j < length - i - 1; j++) {
+          // the element is J and the element next to is is j+ 1
+          if (this.collection[j] > this.collection[j + 1]) {
+            const leftHand = this.collection[j];
+            this.collection[j] = this.collection[j + 1];
+            this.collection[j + 1] = leftHand;
+          }
         }
       }
+    }
+
+    // If logic is a string
+    if (typeof this.collection === "string") {
     }
   }
 }
