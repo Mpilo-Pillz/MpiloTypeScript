82af350d5c312fc719ec0709fadc8a8553caf309
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-02 15:14:27 +0200

    calling the linked list

--- stephen-grider/sort/src/index.ts
+++ stephen-grider/sort/src/index.ts
@@ -1,4 +1,5 @@
 import { CharaterCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { NumbersCollection } from "./NumbersCollection";
 import { Sorter } from "./Sorter";
 
@@ -11,3 +12,16 @@ const charactersCollection = new CharaterCollection("Mpilo");
 const charSorter = new Sorter(charactersCollection);
 charSorter.sort();
 console.log(charactersCollection.data);
+console.log("-------------------------LinbkedList----------------------------");
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+linkedList.add(-550);
+linkedList.add(30);
+linkedList.add(40);
+
+const sorterLinkList = new Sorter(linkedList);
+sorterLinkList.sort();
+linkedList.print();
