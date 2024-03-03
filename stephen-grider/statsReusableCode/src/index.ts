71c1def15a33ea782d20d85baeb3fc511686c4bf
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-03 23:30:44 +0200

    extractign to class

--- stephen-grider/statsReusableCode/src/index.ts
+++ stephen-grider/statsReusableCode/src/index.ts
@@ -1,20 +1,20 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-  .readFileSync("football.csv", {
-    encoding: "utf-8",
-  })
-  .split("\n")
-  .map((row: string): string[] => {
-    return row.split(",");
-  });
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+// this is a colection of closley related values
+enum MatchResult {
+  HomeWin = "H",
+  AwayWin = "A",
+  Draw = "D",
+}
 
 let manUnitedWins = 0;
-
-for (let match of matches) {
-  if (match[1] === "Man United" && match[5] === "H") {
+for (let match of reader.data) {
+  if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
-  } else if (match[2] === "Man United" && match[5] === "A") {
+  } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
     manUnitedWins++;
   }
 }
