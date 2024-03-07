844b8f824c96f7ff33b06c248274e4b372a20c64
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-07 21:21:16 +0200

    using comosed version

--- stephen-grider/statsReusableCode/src/index.ts
+++ stephen-grider/statsReusableCode/src/index.ts
@@ -1,12 +1,21 @@
+import { CsvFileReader } from "./CsvFileReader";
 import { MatchReader } from "./MatchReader";
 import { MatchResult } from "./matchResult";
 
 // const reader = new CsvFileReader("football.csv");
-const reader = new MatchReader("football.csv");
-reader.read();
+// const reader = new MatchReader("football.csv");
+// reader.read();
+
+// Create an object that satisfies the 'dataReader' inderface
+const csvFileReader = new CsvFileReader("football.csv");
+
+// Create an instance of MatchReader and pass in something satisfying
+// the data reader interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of matchReader.matches) {
   if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   } else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
