bfcb8b0c22f9c58e744cfd1c8a37c9c567781c59
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-13 20:29:22 +0200

    creatign summary

--- stephen-grider/statsReusableCode/src/MatchReader.ts
+++ stephen-grider/statsReusableCode/src/MatchReader.ts
@@ -1,7 +1,7 @@
+import { MatchData } from "./MatchData";
 import { MatchResult } from "./matchResult";
 import { dateStringToDate } from "./utils";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
 export interface DataReader {
   read(): void;
   data: string[][];
