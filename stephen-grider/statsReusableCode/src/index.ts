1348141a917ef8eb2ab62088c5a0a7f63b6e414b
Fikasentani <mpilo.dlamini@yahoo.com>
2024-03-14 18:59:43 +0200

    reducing need for imports

--- stephen-grider/statsReusableCode/src/index.ts
+++ stephen-grider/statsReusableCode/src/index.ts
@@ -5,10 +5,12 @@ import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { HtmlReport } from "./reportTargets/HtmlReport";
 
-const csvFileReader = new CsvFileReader("football.csv");
+// const csvFileReader = new CsvFileReader("football.csv");
 
-const matchReader = new MatchReader(csvFileReader);
-matchReader.load();
+// const matchReader = new MatchReader(csvFileReader);
+// matchReader.load();
+
+const matchReader = MatchReader.fromCsv("football.csv");
 
 // const summary = new Summary(new WinsAnalysis("West Ham"), new ConsoleReport());
 // const summary = new Summary(new WinsAnalysis("West Ham"), new HtmlReport());
